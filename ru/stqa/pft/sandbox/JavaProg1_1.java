d2829e15501dde590e2caf71f3c519336c06f8f7
DariaNecht <da.nechiporuk@gmail.com>
2024-08-17 17:25:55 +0300

    #арифметика

--- ru/stqa/pft/sandbox/JavaProg1_1.java
+++ ru/stqa/pft/sandbox/JavaProg1_1.java
@@ -4,5 +4,11 @@ public class JavaProg1_1 {
 
     public static void main(String[] args) {
         System.out.printf("Привет, Даша!!! ");
+        System.out.println(2-2);
+        System.out.println("диджит");
+        System.out.println(1.0/2);
+        System.out.println(2+2*2);
+        System.out.println((2+2)*2);
+        System.out.println("2+2="+(2+2));
     }
 }
